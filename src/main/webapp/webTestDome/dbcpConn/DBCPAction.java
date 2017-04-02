661d56205924ae29a3d38d14f01d1dd3d3c23459
Susie <869397315@qq.com>
2017-04-02 21:20:53 +0800

    Login and new topic are OK!

--- src/main/webapp/webTestDome/dbcpConn/DBCPAction.java
+++ src/main/webapp/webTestDome/dbcpConn/DBCPAction.java
@@ -1,23 +1,27 @@
 package main.webapp.webTestDome.dbcpConn;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Susie on 2017/3/27.
  */
+
 public class DBCPAction {
     //通过DBCP连接池写数据库
-    public static void update(String sql){
+    public static int update(String sql){
         //String sql = "insert into dbcp values ('101')";
+        int updateItemNo=0;
         try {
             System.out.println("========尝试数据库连接========");
             Connection conn = KCYDBCPUtil.getConnection();
             System.out.println("========数据库连接成功========");
             System.out.println("========数据库即将执行："+sql+"========");
             Statement stat = conn.createStatement();
-            stat.executeUpdate(sql);
+            updateItemNo=stat.executeUpdate(sql);
             conn.commit();
             conn.close();
             System.out.println("========数据库执行成功========");
@@ -25,22 +29,43 @@ public class DBCPAction {
             System.out.println("========数据库执行失败========");
             e.printStackTrace();
         }
+        return updateItemNo;
     }
-    public static void query(String sql){
+    public static List<HashMap> query(String sql){
         //String sql = "insert into dbcp values ('101')";
+        ResultSet rs=null;
+        List<HashMap> list=new ArrayList<HashMap>();
         try {
             System.out.println("========尝试数据库连接========");
             Connection conn = KCYDBCPUtil.getConnection();
             System.out.println("========数据库连接成功========");
             System.out.println("========数据库即将执行："+sql+"========");
+            //createStatement 对ResultSet的影响：http://blog.csdn.net/garfielder007/article/details/52053960
             Statement stat = conn.createStatement();
-            stat.executeUpdate(sql);
+            rs=stat.executeQuery(sql);
+            list=convertList(rs);
             conn.commit();
             conn.close();
             System.out.println("========数据库执行成功========");
+            return list;
         } catch (SQLException e) {
             System.out.println("========数据库执行失败========");
             e.printStackTrace();
         }
+        return list;
+    }
+
+    private static List<HashMap> convertList(ResultSet rs) throws SQLException{
+        List<HashMap> list=new ArrayList<HashMap>();
+        ResultSetMetaData md=rs.getMetaData();
+        int columnCount=md.getColumnCount();
+        while (rs.next()){
+            HashMap rowData=new HashMap();
+            for(int i=1;i<=columnCount;i++){
+                rowData.put(md.getColumnName(i),rs.getObject(i));
+            }
+            list.add(rowData);
+        }
+        return list;
     }
 }
